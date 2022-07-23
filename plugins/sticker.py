8e02153ca5d8e9c22a9fcbcc2e6a2820dd11fffe
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-07-23 15:25:49 +0530

    Create sticker.py

---
+++ plugins/sticker.py
@@ -0,0 +1,13 @@
+import os 
+from pyrogram import Client, filters
+
+bot = Client
+
+@bot.on_message(filters.command("stickerid") & ~filters.edited)
+async def sticker_id(_, message: Message):
+    reply = message.reply_to_message
+    if not reply:
+        return await message.reply("Reply to a sticker.")
+    if not reply.sticker:
+        return await message.reply("Reply to a sticker.")
+    await message.reply_text(f"`{reply.sticker.file_id}`")
