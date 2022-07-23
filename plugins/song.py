17fb4b9e99cc56092beac4c8fc953b9db299dd7c
Gavesh Bimsath <91552385+gbimsath@users.noreply.github.com>
2022-07-23 15:14:38 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -1,20 +1,11 @@
-import os
-import requests
-import aiohttp
-import yt_dlp
-import asyncio
-import math
 import time
-import wget
-import aiofiles
-from pyrogram import filters, Client
-from pyrogram.errors import FloodWait, MessageNotModified
-from pyrogram.types import Message
-from youtube_search import YoutubeSearch
-from youtubesearchpython import SearchVideos
-from yt_dlp import YoutubeDL
-import youtube_dl
 import requests
+import yt_dlp as youtube_dl
+from pyrogram import *
+from config import *
+from pyrogram.types import *
+from pyrogram import Client, filters
+from youtube_search import YoutubeSearch
 
 bot = Client
 
